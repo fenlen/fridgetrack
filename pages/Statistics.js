ccd5ae161dcc6a1923a5c8b884f3c86531dcfecd
stevensimpson7 <61119826+stevensimpson7@users.noreply.github.com>
2020-04-01 14:00:36 +0100

    Update Statistics.js
    need help with linking with the database, when using storage.js i do not know how to retrieve the correct data/store it in a usable variable

--- pages/Statistics.js
+++ pages/Statistics.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import {StyleSheet, View, FlatList} from 'react-native';
+import { List, ListItem } from "react-native-elements"; 
 import SubmitButton from '../components/SubmitButton';
 import Style from '../components/Style';
+import storageService from '../services/storage';
+import auth from '@react-native-firebase/auth';
 import {
   Container,
   Header,
@@ -18,8 +22,62 @@ import {
   Row,
   Col
 } from "native-base";
+import Pie from 'react-native-pie';
 
 const Statistics = props => {
+
+  const [results, setResults] = useState();
+
+  useEffect(() => {
+    if (auth().currentUser != null) {
+      storageService.getAllEaten().then(resultList => setResults(resultList));
+      const listLength = (Object.keys(results).length);
+      const truthCounter = 0;
+      const falseCounter = 0;
+
+      var checked = {};
+      var checkedEaten = {};
+      var foodPercentages = {};
+      var sortedPercentages = [];
+      var reversedSortedPercentages = [];
+
+      for (var i = 0; i<listLength; i++) {
+        if (checked.hasOwnProperty(results.i.name) == false) {
+          checked.(results.i.name) = 1;
+          checkedEaten.(results.i.name) = 0;
+          for (var j = i+1; j<=listLength; j++) {
+            if (results.i.name = results.j.name) {
+              checked.(results.i.name)++;
+              if (results.j.eaten == true) {
+                checkedEaten.(results.i.name)++;
+              }
+            }
+          }
+        }
+        if (results.i.eaten == true) {
+          truthCounter++
+        } else {
+          falseCounter++
+        }
+      }
+      const percentage = (truthCounter/(falseCounter+truthCounter))*100;
+
+      for (var k = 0; k<listLength; k++) {
+        foodPercentages[checked.(results.k.name)] = checkedEaten.(results.k.name)/(checkedEaten.(results.k.name)+checked.(results.k.name))*100;
+      }
+      //console.log(foods);
+
+      for (var value in foodPercentages) {
+        sortedPercentages.push([value, foodPercentages[value]]);
+      }
+      sortedPercentages.sort(function(a,b) {
+        return a[1] - b[1];
+      });
+
+      reversedSortedPercentages = sortedPercentages.reverse();
+
+    }
+  }
   return (
         <Container style={Style.container}>
            <Header searchBar>
@@ -37,10 +95,44 @@ const Statistics = props => {
             </Header>
             <Grid>
                 <Row style={{ backgroundColor: '#293b9e' }}>
+                  <View
+                    style={{position: 'absolute', top: 0, left:0, right:0, bottom:0, justifyContent:'center', alignItems:'center'}}
+                  >
+                    <Pie 
+                    radius={100}
+                    innerRadius={90}
+                    sections={[
+                      {
+                        percentage: percentage,
+                        color: '#f00',
+                      },
+                    ]}
+                    backgroundColor="#ddd"
+                    /> 
+                    <Text style={{fontSize: 25, position: 'absolute', top: 130, left:180, right:0, bottom:0, justifyContent:'center', alignItems:'center'}}>{percentage}%</Text>
+                  </View>              
                 </Row>
                 <Row>
-                    <Col style={{ backgroundColor: '#635DB7' }}></Col>
-                    <Col style={{ backgroundColor: '#00CE9F' }}></Col>
+                    <Col style={{ backgroundColor: '#635DB7' }}>
+                      <FlatList 
+                      data={sortedPercentages}
+                      renderItem = {({item}) => (
+                        <ListItem
+                          title={'${item[0]}'}
+                          subtitle={item[1]}
+                        />)}
+                      />
+                    </Col>
+                    <Col style={{ backgroundColor: '#00CE9F' }}>
+                      <FlatList 
+                        data={reversedSortedPercentages}
+                        renderItem = {({item}) => (
+                          <ListItem
+                            title={'${item[0]}'}
+                            subtitle={item[1]}
+                          />)}
+                      />
+                    </Col>
                 </Row>
             </Grid>
             <Footer>
